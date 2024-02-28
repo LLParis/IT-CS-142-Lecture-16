fde242eaa2bf9ba3ac379806bdb8240056dd5b24
Your Name <you@example.com>
2024-02-28 06:19:57 -0800

    Initial Commit

---
+++ src/Lecture16.java
@@ -0,0 +1,33 @@
+public class Lecture16 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #1 here:
+
+    // Put your answer for #2 here:
+
+    // Put your answer for #3 here:
+
+    // Put your answer for #4 here:
+
+    // Put your answer for #5 here:
+
+    // Put your answer for #6 here:
+
+    // Put your answer for #7 here:
+
+    // Put your answer for #8 here:
+
+    // Put your answer for #9 here:
+
+    // EXERCISES:
+
+    // Put your answer for #1 in this repo
+
+    // Put your answer for #2 in this repo
+  }
+}
